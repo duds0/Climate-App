353c5f4cec86af9c5cdf509eb7b4af92e0f887d5
duds0 <eduardofigueiredo1930@gmail.com>
2023-11-14 23:00:25 -0300

    Create and add all background animations

--- lib/backgrounds.dart
+++ lib/backgrounds.dart
@@ -15,7 +15,7 @@ class _Background extends State<Background> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
+        SizedBox(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           child: backgroundAnimation(icon),
@@ -28,37 +28,379 @@ class _Background extends State<Background> {
 backgroundAnimation(String? icon) {
   switch (icon!.toLowerCase()) {
     case "01d":
-      return WrapperScene(
+      return const WrapperScene(
+        colors: [Colors.blue, Colors.lightBlue],
         children: [SunWidget()],
-        colors: [Colors.blue, Colors.white],
       );
 
     case "01n":
-      return WeatherScene.scorchingSun.sceneWidget;
+      return const WrapperScene(colors: [
+        Color(0xff263238),
+        Color(0xff37474f)
+      ], children: [
+        SunWidget(
+          sunConfig: SunConfig(
+            width: 250,
+            blurSigma: 26,
+            coreColor: Color(0xffbdbdbd),
+            midColor: Color(0x18fafafa),
+            outColor: Color(0x00757575),
+          ),
+        )
+      ]);
 
     case "02d":
-      return WeatherScene.showerSleet.sceneWidget;
+      return const WrapperScene(colors: [
+        Color(0xff303f9f),
+        Color(0xff1e88e5),
+        Color(0xffbdbdbd)
+      ], children: [
+        SunWidget(
+          sunConfig: SunConfig(
+            coreColor: Color(0xffffb74d),
+            midColor: Color(0xffffff8d),
+            outColor: Color(0xffffd180),
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 160,
+            x: 140,
+            y: 97,
+            color: Color(0xa8fafafa),
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 250,
+            color: Color(0xa8fafafa),
+            x: 20,
+            y: 3,
+          ),
+        )
+      ]);
 
     case "02n":
-      return WeatherScene.showerSleet.sceneWidget;
+      return const WrapperScene(colors: [
+        Color(0xff263238),
+        Color(0xff37474f)
+      ], children: [
+        SunWidget(
+          sunConfig: SunConfig(
+            width: 250,
+            blurSigma: 26,
+            coreColor: Color(0xffbdbdbd),
+            midColor: Color(0x18fafafa),
+            outColor: Color(0x00757575),
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 250,
+            color: Color(0x879e9e9e),
+            x: 20,
+            y: 35,
+            scaleBegin: 1,
+            scaleEnd: 1.08,
+            slideX: 20,
+            slideY: 0,
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 160,
+            color: Color(0x65d6d6d6),
+            x: 140,
+            y: 130,
+            scaleBegin: 1,
+            scaleEnd: 1.1,
+            slideX: 20,
+            slideY: 4,
+          ),
+        )
+      ]);
 
-    case "03d" || "03n" || "04d" || "04n":
-      return WeatherScene.frosty.sceneWidget;
+    case "03d":
+      return const WrapperScene(colors: [
+        Color(0xff3d5afe),
+        Color(0xff2196f3)
+      ], children: [
+        SunWidget(
+          sunConfig: SunConfig(
+              width: 300,
+              midColor: Color(0x00000000),
+              outColor: Color(0x00000000),
+              coreColor: Color(0xffffb74d)),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 269,
+            color: Color(0xbfeeeeee),
+            x: 22,
+            y: 3,
+            scaleBegin: 1,
+            scaleEnd: 1.08,
+            slideX: 20,
+            slideY: 0,
+          ),
+        ),
+      ]);
 
-    case "09d" || "10d":
+    case "03n":
+      return const WrapperScene(colors: [
+        Color(0xff263238),
+        Color(0xff37474f)
+      ], children: [
+        SunWidget(
+          sunConfig: SunConfig(
+            width: 300,
+            coreColor: Color(0xffbdbdbd),
+            midColor: Color(0x00ffee58),
+            outColor: Color(0x02ffa726),
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 269,
+            color: Color(0xbfeeeeee),
+            x: 22,
+            y: 3,
+            scaleBegin: 1,
+            scaleEnd: 1.08,
+            slideX: 20,
+            slideY: 0,
+          ),
+        ),
+      ]);
+
+    case "04d":
+      return const WrapperScene(colors: [
+        Color(0xff3d5afe),
+        Color(0xff2196f3)
+      ], children: [
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 250,
+            color: Color(0xbfeeeeee),
+            x: 20,
+            y: 35,
+            scaleBegin: 1,
+            scaleEnd: 1.08,
+            slideX: 20,
+            slideY: 0,
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 160,
+            color: Color(0x8be0e0e0),
+            x: 140,
+            y: 130,
+            scaleBegin: 1,
+            scaleEnd: 1.1,
+            slideX: 20,
+            slideY: 4,
+          ),
+        ),
+      ]);
+
+    case "04n":
+      return const WrapperScene(colors: [
+        Color(0xff263238),
+        Color(0xff78909c)
+      ], children: [
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 250,
+            color: Color(0xad90a4ae),
+            x: 20,
+            y: 35,
+            scaleBegin: 1,
+            scaleEnd: 1.08,
+            slideX: 20,
+            slideY: 0,
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 160,
+            color: Color(0xb1607d8b),
+            x: 140,
+            y: 130,
+            scaleBegin: 1,
+            scaleEnd: 1.1,
+            slideX: 20,
+            slideY: 4,
+          ),
+        ),
+      ]);
+
+    case "09d" || "09n":
       return WeatherScene.rainyOvercast.sceneWidget;
 
-    case "09n" || "10n":
-      return WeatherScene.rainyOvercast.sceneWidget;
+    case "10d":
+      return const WrapperScene(colors: [
+        Color(0xff616161),
+        Color(0xffbdbdbd)
+      ], children: [
+        SunWidget(
+          sunConfig: SunConfig(
+            width: 278,
+            blurSigma: 26,
+            coreColor: Color(0xffffcc80),
+            midColor: Color(0x38fff176),
+            outColor: Color(0x00000000),
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 250,
+            color: Color(0xea424242),
+            x: 20,
+            y: 35,
+            scaleBegin: 1,
+            scaleEnd: 1.08,
+            slideX: 20,
+            slideY: 0,
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 160,
+            color: Color(0xe1303030),
+            x: 140,
+            y: 130,
+            scaleBegin: 1,
+            scaleEnd: 1.1,
+            slideX: 20,
+            slideY: 4,
+          ),
+        ),
+        RainWidget(
+          rainConfig: RainConfig(
+            count: 10,
+            lengthDrop: 8,
+            widthDrop: 4,
+            color: Color(0xdb455a64),
+            fallRangeMinDurMill: 500,
+            fallRangeMaxDurMill: 1500,
+            areaXStart: 84,
+            areaXEnd: 191,
+            areaYStart: 215,
+            areaYEnd: 540,
+            slideX: 2,
+            slideY: 0,
+          ),
+        ),
+        RainWidget(
+          rainConfig: RainConfig(
+            count: 10,
+            lengthDrop: 13,
+            widthDrop: 5,
+            color: Color(0xdb455a64),
+            areaXStart: 51,
+            areaXEnd: 199,
+            areaYStart: 199,
+            slideX: 2,
+            slideY: 0,
+          ),
+        )
+      ]);
+
+    case "10n":
+      return const WrapperScene(colors: [
+        Color(0xff263238),
+        Color(0xff37474f)
+      ], children: [
+        SunWidget(
+          sunConfig: SunConfig(
+            width: 250,
+            blurSigma: 26,
+            coreColor: Color(0xffbdbdbd),
+            midColor: Color(0x18fafafa),
+            outColor: Color(0x00757575),
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 250,
+            color: Color(0xcc616161),
+            x: 20,
+            y: 35,
+            scaleBegin: 1,
+            scaleEnd: 1.08,
+            slideX: 20,
+            slideY: 0,
+          ),
+        ),
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 160,
+            color: Color(0xff616161),
+            x: 140,
+            y: 130,
+            scaleBegin: 1,
+            scaleEnd: 1.1,
+            slideX: 20,
+            slideY: 4,
+          ),
+        ),
+        RainWidget(
+          rainConfig: RainConfig(
+            count: 10,
+            lengthDrop: 8,
+            widthDrop: 4,
+            color: Color(0xff78909c),
+            fallRangeMinDurMill: 500,
+            fallRangeMaxDurMill: 1500,
+            areaXStart: 84,
+            areaXEnd: 191,
+            areaYStart: 215,
+            areaYEnd: 540,
+            slideX: 2,
+            slideY: 0,
+          ),
+        ),
+        RainWidget(
+          rainConfig: RainConfig(
+            count: 10,
+            lengthDrop: 13,
+            widthDrop: 5,
+            color: Color(0x9978909c),
+            areaXStart: 51,
+            areaXEnd: 199,
+            areaYStart: 199,
+            slideX: 2,
+            slideY: 0,
+          ),
+        )
+      ]);
 
     case "11d" || "11n":
       return WeatherScene.stormy.sceneWidget;
 
     case "13d" || "13n":
-      return WeatherScene.frosty.sceneWidget;
+      return WeatherScene.showerSleet.sceneWidget;
 
     case "50d" || "50n":
-      return WeatherScene.weatherEvery.sceneWidget;
+      return const WrapperScene(colors: [
+        Color(0xff616161),
+        Color(0xffbdbdbd)
+      ], children: [
+        CloudWidget(
+          cloudConfig: CloudConfig(
+            size: 250,
+            color: Color(0xffbdbdbd),
+            x: 20,
+            y: 35,
+            scaleBegin: 1,
+            scaleEnd: 1.08,
+            slideX: 20,
+            slideY: 0,
+          ),
+        ),
+        WindWidget()
+      ]);
 
     default:
       return WeatherScene.weatherEvery.sceneWidget;
