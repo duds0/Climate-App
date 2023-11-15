cb99d44e874d2633a3e9a109e6949faa288bb8bf
duds0 <eduardofigueiredo1930@gmail.com>
2023-11-15 11:25:32 -0300

    Improve TextField design

--- lib/main.dart
+++ lib/main.dart
@@ -28,39 +28,38 @@ class _MyApp extends State<MyApp> {
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
               child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Colors.white),
-                        labelText: "Digite sua cidade aqui",
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(16),
-                          ),
-                        ),
-                        // -------
-                        // -------
-                        // -------
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(16),
-                          ),
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      labelStyle: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w300),
+                      labelText: "Digite sua cidade aqui",
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16),
                         ),
                       ),
-                      onSubmitted: (value) {
-                        setState(
-                          () {
-                            cityValue = value;
-                          },
-                        );
-                      },
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                      ),
                     ),
-                  )),
+                    onSubmitted: (value) {
+                      setState(
+                        () {
+                          cityValue = value;
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
             ),
           ],
         ),
