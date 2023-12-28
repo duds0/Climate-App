54f721aa596b8bcb5480e68e60a96cb4ef0b86bc
duds0 <eduardofigueiredo1930@gmail.com>
2023-12-28 07:13:02 -0300

    Moves global variables and import. Simple refactor

--- lib/widgets/principal_informations.dart
+++ lib/widgets/principal_informations.dart
@@ -1,13 +1,9 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names
 import 'package:climate_app/services/api_openweather.dart';
 import '../models/backgrounds.dart';
-import 'package:climate_app/main.dart';
+import 'package:climate_app/global/variables.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-
-var icon = "initial";
-var snap;
-var requests;
 
 class MainScreen extends StatefulWidget {
   final String city;
