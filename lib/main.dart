94ca0af86c9beab3d035fa2d8eb2dd5b80702b23
duds0 <eduardofigueiredo1930@gmail.com>
2023-12-28 07:07:31 -0300

    Remove a global variable

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:climate_app/screens/new_locations.dart';
 import 'screens/home.dart';
 import 'package:flutter/material.dart';
 
-// ignore: prefer_typing_uninitialized_variables
-var cityValue = "";
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
@@ -12,8 +9,6 @@ class MyApp extends StatefulWidget {
   @override
   // ignore: library_private_types_in_public_api
   _MyApp createState() => _MyApp();
-
-  // This widget is the root of your application.
 }
 
 class _MyApp extends State<MyApp> {
