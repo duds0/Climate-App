e1e0f30f27a3ed4656a510a53e495973e9c8f425
duds0 <eduardofigueiredo1930@gmail.com>
2024-01-16 17:40:07 -0300

    adds controller variables

--- lib/global/variables.dart
+++ lib/global/variables.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 List<String> cities = [];
 List items = [];
+bool homePageController = true;
+bool isFirstTimePressed = true;
 var icon = "initial";
 var snap;
 var requests;
