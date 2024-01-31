7820b3e78d2656d0e14b6010ec57dc02db9fde3b
duds0 <eduardofigueiredo1930@gmail.com>
2024-01-31 00:52:32 -0300

    moves the latitude and longitude variables and creates a variable for dates

--- lib/global/variables.dart
+++ lib/global/variables.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 List<String> cities = [];
+List<String> dates = [];
 List items = [];
 bool homePageController = true;
 bool isFirstTimePressed = true;
+late var lat;
+late var lon;
 var cityName;
 var icon = "initial";
 var snap;
