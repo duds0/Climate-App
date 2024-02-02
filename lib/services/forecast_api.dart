1ef3449f8a42db590788b41f19b834e312950bc6
duds0 <eduardofigueiredo1930@gmail.com>
2024-02-02 19:07:55 -0300

    Refactor and Fix: Improves the logic and avoids double returns with 2 functions in addition to already returning the name of the week.

--- lib/services/forecast_api.dart
+++ lib/services/forecast_api.dart
@@ -1,31 +1,66 @@
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import "dart:async";
+import "dart:convert";
+import "package:http/http.dart" as http;
 import "package:climate_app/global/variables.dart";
 
 Future fetchForecast() async {
   var url =
       "https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&appid=95b9027fe9d1f6c19c6b21c7a2d3f521&units=metric&lang=pt_br";
   var response = await http.get(Uri.parse(url));
-  var json = jsonDecode(response.body);
-  List<Map<String, dynamic>> dailyForecasts = [];
-  String completedDate = DateTime.now().toString();
-  String date = completedDate.split(" ")[0];
+  var jsonData = jsonDecode(response.body);
 
-  for (var item in json["list"]) {
-    String dateStr = item["dt_txt"].split(' ')[0];
-    int hour = int.parse(item["dt_txt"].split(' ')[1].split(':')[0]);
+  List<Map<String, dynamic>> forecast = [];
 
-    if (date != dateStr && hour >= 12) {
-      if (!dailyForecasts.any((forecast) => forecast['date'] == dateStr)) {
-        dailyForecasts.add({
-          'date': dateStr,
-          'forecast': item,
-        });
-        dates.add(item["dt_txt"]);
+  List<dynamic> forecastList = jsonData["list"];
+
+  for (int i = 0; i < forecastList.length; i += 8) {
+    String dateTimeString = forecastList[i]["dt_txt"];
+    DateTime date = DateTime.parse(dateTimeString);
+    String dayOfWeek = getDayOfWeek(date.weekday);
+
+    double totalTemp = 0;
+    int countTemp = 0;
+
+    for (int j = i; j < i + 8; j++) {
+      if (j < forecastList.length) {
+        totalTemp += forecastList[j]["main"]["temp"];
+        countTemp++;
       }
     }
+
+    double averageTemp = countTemp > 0 ? totalTemp / countTemp : 0;
+
+    String icon = forecastList[i + 4]["weather"][0]["icon"];
+
+    Map<String, dynamic> dayForecast = {
+      "weekDay": dayOfWeek,
+      "averageTemp": averageTemp,
+      "icon": icon,
+    };
+
+    forecast.add(dayForecast);
   }
 
-  return dailyForecasts;
+  return forecast;
+}
+
+String getDayOfWeek(int day) {
+  switch (day) {
+    case 1:
+      return "Seg.";
+    case 2:
+      return "Ter.";
+    case 3:
+      return "Qua.";
+    case 4:
+      return "Qui.";
+    case 5:
+      return "Sex.";
+    case 6:
+      return "Sáb.";
+    case 7:
+      return "Dom.";
+    default:
+      return "";
+  }
 }
