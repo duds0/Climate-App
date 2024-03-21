419ccb6d2c0a4e571d448aaf68b84e2b3f891076
duds0 <eduardofigueiredo1930@gmail.com>
2024-03-21 10:49:34 -0300

    Improves style

--- lib/widgets/additional_informations.dart
+++ lib/widgets/additional_informations.dart
@@ -15,32 +15,35 @@ class AdditionalInformations extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          description,
-          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-        ),
-        const SizedBox(height: 8),
-        RichText(
-          text: TextSpan(
-            children: [
-              TextSpan(
-                text: content,
-                style: const TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextSpan(
-                text: unitOfMeasurement,
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-            ],
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          Text(
+            description,
+            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
           ),
-        ),
-      ],
+          const SizedBox(height: 8),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: content,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: unitOfMeasurement,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
