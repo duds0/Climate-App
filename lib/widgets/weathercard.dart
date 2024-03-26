4fa19df4c53c7a12a29f22a539bf896215ed9731
duds0 <eduardofigueiredo1930@gmail.com>
2024-03-26 18:05:46 -0300

    Shimmer effect

--- lib/widgets/weathercard.dart
+++ lib/widgets/weathercard.dart
@@ -5,6 +5,7 @@ import 'package:climate_app/widgets/principal_informations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:shimmer/shimmer.dart';
 
 // ignore: must_be_immutable
 class WeatherCard extends StatefulWidget {
@@ -97,12 +98,15 @@ class _WeatherCard extends State<WeatherCard> {
           );
         } else if (snapshot.connectionState == ConnectionState.waiting &&
             cityValue != "") {
-          return const SizedBox(
+          return SizedBox(
             height: 120,
-            child: Center(
-              child: CircularProgressIndicator(
-                strokeWidth: 3,
-                color: Color(0xffffffff),
+            child: Shimmer.fromColors(
+              baseColor: Colors.white.withOpacity(0.2),
+              highlightColor: Colors.grey.withOpacity(0.2),
+              child: Container(
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.white),
               ),
             ),
           );
