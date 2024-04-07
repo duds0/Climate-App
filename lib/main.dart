8165f6786093e46dc4beb53e992a3f4a802e4b13
duds0 <eduardofigueiredo1930@gmail.com>
2024-04-07 17:57:27 -0300

    Inter font

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
   @override
   // ignore: library_private_types_in_public_api
   _MyApp createState() => _MyApp();
@@ -15,7 +15,8 @@ class _MyApp extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData(fontFamily: 'Inter'),
       initialRoute: "/home",
       routes: {
         "/home": (context) => const HomePage(),
